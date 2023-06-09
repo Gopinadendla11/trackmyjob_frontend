5cc3362da3e4ab7738ab514cbe6bafe9c4039b18
Gopi Krishna Nadendla <43063987+Gopinadendla11@users.noreply.github.com>
2023-06-09 15:08:01 -0400

    Update env.js

--- trackmyjobapp/src/env.js
+++ trackmyjobapp/src/env.js
@@ -1,2 +1,2 @@
-//export const BASE_API_URL = "https://trackmyjob-server.onrender.com";
-export const BASE_API_URL = "http://3.93.51.231:5000";
+export const BASE_API_URL = "https://trackmyjob-server.onrender.com";
+//export const BASE_API_URL = "http://3.93.51.231:5000";
